71be0cee26cbe5379cb9b4f6dc9182df4d36747c
Bryce Hoehn <bryce.hoehn@icloud.com>
2026-04-27 21:56:50 -0400

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "{{ name.capitalize() }} distroless image";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = builtins.currentSystem;
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {
+      {{ name }}-image = pkgs.dockerTools.buildLayeredImage {
+        name = "{{ name }}";
+        tag = "latest";
+        contents = [ 
+          pkgs.{{ name }}
+        ];
+        config = {
+          ExposedPorts = {
+            "1234/tcp" = {};
+          };
+          Volumes = {
+            "/config" = {};
+            "/data" = {};
+          };
+
+          Cmd = [ "${pkgs.{{ name }}}/bin/{{ name }}" ];
+          # Distroless non‑root user
+          User = "1000";
+          WorkingDir = "/config";
+        };
+      };
+    };
+
+    # Expose the {{ name }} version for CI workflows
+    {{ name }}Version = pkgs.{{ name }}.version;
+
+    defaultPackage.${system} = self.packages.${system}.{{ name }}-image;
+  };
+}
