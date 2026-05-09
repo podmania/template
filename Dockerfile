c38514f750e9c7546bd9a3337687de77bef74838
bryce <bryce.hoehn@icloud.com>
2026-05-09 16:20:19 -0400

    feat: add debug image variant with shared base-debug layer

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,14 @@
 FROM nixos/nix:latest AS builder
 RUN printf "extra-experimental-features = nix-command flakes\n" >> /etc/nix/nix.conf
-WORKDIR /build
-COPY flake.nix flake.lock ./
-RUN nix build .#<%= name %> --impure --out-link /build/result
+
+ARG REPO=podmania/<%= name %>
+RUN nix build "github:${REPO}#<%= name %>" --impure --out-link /build/result
 RUN mkdir -p /rootfs/nix/store && \
     for path in $(nix-store -qR /build/result); do \
       cp -a "$path" "/rootfs$path"; \
     done && \
     cp -aL /build/result "/rootfs/nix/store/app"
 
-FROM scratch
+FROM ghcr.io/podmania/base:latest
 COPY --from=builder /rootfs /
 ENTRYPOINT ["/nix/store/app/bin/<%= main_program or name %>"<% for arg in cmd_args %>, "<%= arg %>"<% endfor %>]
--- flake.nix
+++ flake.nix
@@ -11,32 +11,40 @@
     system = builtins.currentSystem;
     pkgs = nixpkgs.legacyPackages.${system};
     n2c = nix2container.outputs.packages.${system}.nix2container;
+    imageConfig = {
+      ExposedPorts = {
+        <% for port in ports %>
+        "<%= port %>/tcp" = {};
+        <% endfor %>
+      };
+      Volumes = {
+        <% for volume in volumes %>
+        "<%= volume %>" = {};
+        <% endfor %>
+      };
+      <% if env %>
+      Env = [
+        <% for e in env %>
+        "<%= e %>"
+        <% endfor %>
+      ];
+      <% endif %>
+      Cmd = [ "${pkgs.<%= name %>}/bin/<%= main_program or name %>"<% for arg in cmd_args %> "<%= arg %>"<% endfor %> ];
+    };
   in {
     packages.${system} = {
       <%= name %>-image = n2c.buildImage {
         name = "<%= name %>";
         tag = "latest";
         fromImage = base.packages.${system}.base-image;
-        config = {
-          ExposedPorts = {
-            <% for port in ports %>
-            "<%= port %>/tcp" = {};
-            <% endfor %>
-          };
-          Volumes = {
-            <% for volume in volumes %>
-            "<%= volume %>" = {};
-            <% endfor %>
-          };
-          <% if env %>
-          Env = [
-            <% for e in env %>
-            "<%= e %>"
-            <% endfor %>
-          ];
-          <% endif %>
-          Cmd = [ "${pkgs.<%= name %>}/bin/<%= main_program or name %>"<% for arg in cmd_args %> "<%= arg %>"<% endfor %> ];
-        };
+        config = imageConfig;
+      };
+
+      <%= name %>-debug-image = n2c.buildImage {
+        name = "<%= name %>";
+        tag = "latest-debug";
+        fromImage = base.packages.${system}.base-debug-image;
+        config = imageConfig;
       };
 
       <%= name %> = pkgs.<%= name %>;
